6ab5e709ebb3d15d8f3e849f97291fe4d520f653
Kalvyn Lu <kalvynlu@gmail.com>
2014-11-02 19:01:26 -0700

    Paintbucket/IO

---
+++ src/ColorPicker/Tools/IO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ColorPicker.Tools;
+
+/**
+ *
+ * @author ninjakl
+ */
+
+import java.awt.*;
+import java.io.File;
+import javax.imageio.ImageIO;
+        
+public class IO {
+    
+}
--- src/ColorPicker/Tools/PaintBucket.java
+++ src/ColorPicker/Tools/PaintBucket.java
@@ -9,6 +9,9 @@ package ColorPicker.Tools;
 import Util.Point;
 import java.awt.Color;
 import java.awt.Image;
+import Util.Delta;
+import java.awt.Graphics;
+import java.util.ArrayList;
 
 /**
  *
@@ -21,25 +24,38 @@ public class PaintBucket implements Tool {
      * This function has to apply some change to colorArr
      * Color c is curColor in GridPanel
      */
-    public void apply(Color c, int x, int y, Color[][] colorArr) {
-        Color curColor = colorArr[x][y];
+    public ArrayList<Delta> apply(Color c, int x, int y, Color[][] colorArr) {
+        ArrayList<Delta> deltaMap = new ArrayList<>();
+        bucketHelper(c,x,y,colorArr,deltaMap);   
+        return deltaMap;
+    }
+    
+    public void bucketHelper(Color c, int x, int y, Color[][] colorArr, ArrayList<Delta> deltaMap){
+        Color curColor = colorArr[x][y];       
+        if(curColor == c) return;
+
         colorArr[x][y] = c;
         
-        //base case
-        if(c == curColor) return;
+        deltaMap.add(new Delta(x,y,curColor,c));
+        
         
         if(x > 0 && colorArr[x - 1][y] == curColor){
-            apply(c,x - 1,y,colorArr);
+            bucketHelper(c,x - 1,y,colorArr,deltaMap);
         }
         if(x < colorArr[0].length - 1&& colorArr[x + 1][y] == curColor){
-            apply(c,x + 1,y,colorArr);
+            bucketHelper(c,x + 1,y,colorArr,deltaMap);
         }
         if(y > 0 && colorArr[x][y - 1] == curColor){
-            apply(c,x,y - 1,colorArr);
+            bucketHelper(c,x,y - 1,colorArr,deltaMap);
         }
         if(y < colorArr.length - 1&& colorArr[x][y + 1] == curColor){
-            apply(c,x,y + 1,colorArr);
+            bucketHelper(c,x,y + 1,colorArr,deltaMap);
         }
+    }
+
+    @Override
+    public void paintSelf(Graphics g, int pixelSize) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
